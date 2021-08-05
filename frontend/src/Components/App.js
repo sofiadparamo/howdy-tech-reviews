61651511523f2613ccafb9950eea890ae94baf6b
Josue Rosales <jrosa1756@gmail.com>
2021-08-05 12:43:54 -0500

    Added Page Routing & Blank Home and Login Pages (#14)

---
+++ frontend/src/Components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavigationBar from './NavigationBar';
+import Main from './Main';
+
+// Renders the NavBar and other screens together
+
+const App = () => (
+  <div>
+    <NavigationBar />
+    <Main />
+  </div>
+)
+
+export default App
