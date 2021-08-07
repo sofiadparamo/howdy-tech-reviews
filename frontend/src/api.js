e75c734603ce9ceb721a415d3a298f2036e4415b
coffeemates <jrosa1756@gmail.com>
2021-08-06 19:31:35 -0500

    Added Listing Backend (POST, GET)

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,12 @@
+/*
 const ListingAPI = {
   listings: [
     { id: 1, productName: "iPhone", rating: 4 },
     { id:2, productName: "Slack", rating: 3 },
-    { id:3, productName: "Arselan", rating: 5 }
-  ],
+    { id:3, productName: "Arsalan", rating: 5 }
+  ]
+  
+  listings: ListingService.getListings(),
   all: function() { return this.listings },
   get: function(id) {
     const isListing = p => p.id === id
@@ -12,3 +15,18 @@ const ListingAPI = {
 }
 
 export default ListingAPI
+*/
+
+class ListingAPI {
+  constructor() {
+    this.state = {
+      listings: []
+    }
+  }
+
+  all() {
+    ListingService.getListings().then( res => {
+      this.setState({ listings: this.state.listings });
+    });
+  }
+}
