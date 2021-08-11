bf81163931b7d53e223a3313c7102c15f0670443
coffeemates <jrosa1756@gmail.com>
2021-08-11 15:39:39 -0500

    Added product posting

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,10 +3,6 @@ import axios from 'axios'
 const LISTING_API_URL = "http://localhost:5000/listings";
 
 class ListingAPI {
-  constructor() {
-    this.listings = axios.get(LISTING_API_URL);
-  }
- 
   all() {
     return axios.get(LISTING_API_URL);
   }
@@ -14,7 +10,31 @@ class ListingAPI {
   get(id) {
     return axios.get(LISTING_API_URL + '/' + id);
   }
-}
 
+  post(productName, productDescription, rating) {
+    var data = JSON.stringify({
+      "productName": productName,
+      "productDescription": productDescription,
+      "rating": rating
+    });
+
+    var config = {
+      method: 'post',
+      url: 'http://localhost:5000/listings',
+      headers: { 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+}
 const ListingsAPI = new ListingAPI();
 export default ListingsAPI
