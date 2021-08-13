bfbef9a8be12e06400fe5cdb257716eba036189b
CaroCardozo <ccardozo1234@gmail.com>
2021-08-13 18:20:58 -0500

    First insights of the opinions database #5

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -83,3 +83,88 @@ def one_item(item_id):
 @app.route("/")
 def index():
     return "Works :)", 200
+
+#DB for the people opinions/posts
+@app.route("/opinion", methods=["POST", "GET"])
+def opinions_list():
+    if request.method == "POST":
+        body = request.json
+        product_name = body["productName"]
+        product_opinion = body["productOpinion"]
+        product_link = body["poductLink"]
+        _user = body["user"]
+        rating = body["rating"]
+
+        db["opinions"].insert_one(
+            {
+                "productName": product_name,
+                "productOpinion": product_opinion,
+                "productLink": product_link,
+                "_user": user,
+                "rating": rating,
+            }
+        )
+        return jsonify(
+            {
+                "status": "Data is posted to MongoDB!",
+                "productName": product_name,
+                "productOpinion": product_opinion,
+                "productLink": product_link,
+                "_user": user,
+                "rating": rating,
+            }
+        )
+
+    if request.method == "GET":
+        all_data = db["opinions"].find()
+        data_json = []
+        for data in all_data:
+            product_id = data["_id"]
+            product_name = data["productName"]
+            product_opinion = data["productOpinion"]
+            product_link = data["productLink"]
+            user = data["_user"]
+            rating = data["rating"]
+            data_dict = {
+                "id": str(product_id),
+                "productName": product_name,
+                "productOpinion": product_opinion,
+                "productLink": product_link,
+                "_user": user,
+                "rating": rating,
+            }
+            data_json.append(data_dict)
+        print(data_json)
+        return jsonify(data_json)
+
+
+@app.route("/post/<item_id>", methods=["GET"])
+def one_opinion(item_id):
+    if request.method == "GET":
+        opinions_of_product = []
+        product = db["opinions"].find({"_id": ObjectId(item_id)})
+        item_id = product_name = product_opinion = product_link = user = rating = []
+        for opinion in product:
+            item_id.append(opinion["_id"])
+            product_name.append(opinion["productName"])
+            product_opinion.append(opinion["productOpinion"])
+            product_link.append(opinion["productLink"])
+            user.append(opinion["_user"])
+            rating.append(opinion["rating"])
+            data_dict = {
+                "id": str(item_id),
+                "productName": product_name,
+                "productOpinion": product_opinion,
+                "productLink": product_link,
+                "_user": user,
+                "rating": rating,
+            }
+            print(data_dict)
+            opinions_of_product.append(jsonify(data_dict))
+        return opinions_of_product
+
+@app.route("/post/<item_id>", methods=["DELETE"])
+def del_post(item_id, user):
+    db["opinions"].delete_many({"_id": ObjectId(item_id),"_user": user})
+    print("\n# Deletion successful # \n")
+    return jsonify({"status": "Data id: " + item_id + "of user " + user + " is deleted!"})
