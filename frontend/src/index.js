6d83e63566b2c0c39c215b8eb76d065b4ddc56f2
Carolina Cardozo <66146408+CaroCardozo@users.noreply.github.com>
2021-08-17 16:35:45 -0500

    Add Sentry monitoring for React frontend dev (#46)
    Co-authored-by: SweetSofiMC <sweetsofimc@gmail.com>

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './Components/App';
+
+Sentry.init({
+  dsn: "https://3b4279d3c3814f89884479ac04aa2ab5@o961376.ingest.sentry.io/5909788",
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 render((
   <BrowserRouter>
