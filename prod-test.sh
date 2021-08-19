1fd4348cca3672e3a0de5dfccd1277e0a37d30b1
coffeemates <jrosa1756@gmail.com>
2021-08-19 10:05:08 -0500

    Fixed crash issue

--- prod-test.sh
+++ prod-test.sh
@@ -8,18 +8,21 @@ resp=$(curl -s -w "%{http_code}" GET $url)
 result=${resp: -3}
 if [[ $result == 200 ]]; then
     echo "TEST Index: PASS"
-    url="https://howdytech.duckdns.org:5000"    
+    url="http://howdytech.duckdns.org:5000"    
     resp=$(curl -s -w "%{http_code}" GET $url)
     result=${resp: -3}
 
     if [[ $result == 200 ]]; then
         echo "TEST Db: PASS"
+        sleep 3
         exit 0
     else
         echo "TEST Db: FAIL, Result: $result"
+        sleep 3
         exit 1
     fi
 else
     echo "TEST Index: FAIL, Result: $result"
+    sleep 3
     exit 1
 fi
