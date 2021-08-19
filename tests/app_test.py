ae99535df1c3b7eb9933c77ccff74746f3a0671d
coffeemates <jrosa1756@gmail.com>
2021-08-19 09:49:38 -0500

    Added basic python testing

--- tests/app_test.py
+++ tests/app_test.py
@@ -2,9 +2,22 @@ import requests
 import pytest
 
 def test_index():
-    response = requests.get("https://howdytech.duckdns.org");
-    assert response.status_code == 200
+    try:
+        response = requests.get("https://howdytech.duckdns.org");
+        assert response.status_code == 200
+        print("Testing Index Page: PASS")
+    except requests.exceptions.RequestException as e:
+        print("Testing Index Page: FAIL")
+        raise SystemExit(e)
 
 def test_database():
-    response = requests.get("http://howdytech.duckdns.org:5000");
-    assert response.status_code == 200
+    try:
+        response = requests.get("http://howdytech.duckdns.org:5000");
+        assert response.status_code == 200
+        print("Testing Database: PASS")
+    except requests.exceptions.RequestException as e:
+        print("Testing Database: FAIL")
+        raise SystemExit(e)
+
+test_index()
+test_database()
